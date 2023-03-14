063381f96c161db6946ab94400e5ae03e92cadac
Sam Crabtree <samuel.crabtree@protonmail.com>
2023-03-14 00:49:43 -0400

    Tests Added
    Adding commit for peer review.

--- message_board/m_board/tests.py
+++ message_board/m_board/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from .models import Post
 # Create your tests here.
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.Objects.create(text="This is a test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.Post.text, "This is a test!")
