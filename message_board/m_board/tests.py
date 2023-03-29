2ba6761a9e68f8289d27f9dc135040ccba546381
Sam Crabtree <samuel.crabtree@protonmail.com>
2023-03-29 17:15:56 -0400

    Retry.
    Retry heroku steps

--- message_board/m_board/tests.py
+++ message_board/m_board/tests.py
@@ -15,18 +15,6 @@ class PostTests(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    # def test_url_available_by_name(self):
-    #     response = self.client.get(reverse("home"))
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_template_name_correct(self):
-    #     response = self.client.get(reverse('home'))
-    #     self.assertTemplateUsed(response, 'post_list.html')
-    #
-    # def test_template_content(self):
-    #     response = self.client.get(reverse("home"))
-    #     self.assertContains(response, "This is a test!")
-
     def test_homepage(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
